f9b9b91e30936d2c5f9982214e2a7eb51e83db2f
renegadelha <renegadelha@gmail.com>
2023-02-19 16:30:49 -0300

    voltei sem pages

--- test7.py
+++ test7.py
@@ -1,16 +1,14 @@
 import analisador
 import datetime
-import dash
 import pandas as pd
 import pathlib
-from dash import Dash, html, dcc, dash_table, State, exceptions, callback
+from dash import Dash, html, dcc, dash_table, State, exceptions
 from dash.dependencies import Input, Output
 import plotly.express as px
 import graphViewer as gv
 import plotly.graph_objs as go
-from pages import secondPage, firstPage
+from pages import secondPage
 
-import manipulatecsv
 
 def gerarTable(tdiv, valor):
     dadosTable = analisador.distribuirAporte(tdiv.copy(), valor)
@@ -35,47 +33,107 @@ def viewTableTop(tdiv):
     #return gerarTable(tdiv.head(int(len(tdiv) // 1.5)), 1000)
 
 
-app = Dash(__name__, suppress_callback_exceptions=True)
+app = Dash(__name__)
 server = app.server #server heroku reconhecer a app
 
 
+actual_dir = pathlib.Path().absolute()
+
+path = f'{actual_dir}/data/statusinvest-busca-avancada.csv'
+data_cias = analisador.lerCsv(path)
+tdiv = pd.DataFrame()
+figura = None
+
 def serve_layout():
+    global tdiv
+    global figura
+    global data_cias
+
+    tdiv = gerarTdiv(3, data_cias)
+#   fig = px.line(figura)
+
+    tableAll = viewTableAll(tdiv)
+    tableTop = viewTableTop(tdiv)
+
 
     return html.Div(children=[
         html.Div([html.H1(children='DATA FINANCE')]
                  , className='banner'
                  )
         ,
+        html.Div([html.H3(children='Top')]
+                 , className='banner1'),
+        html.Div(['valor do Aporte:',
+                  dcc.Input(id='botaoValor', value='1000', type='text'),
+                  html.Button(id='botaoInput', n_clicks=0, children='Atualizar')
+                  ])
+        ,
+        html.Div(id='dataframe_output',
+                 children=html.Div([
+                                    dash_table.DataTable(
+                                        id='table',
+                                        data=tableAll.to_dict('records'),
+                                        columns=[{"name": i, "id": i} for i in tableAll.columns],
+                                    )
+                                    ])
+                 )
+        ,
+        html.Br(),
+        html.Div([html.H3(children='Top +')]
+                 , className='banner1'),
+        dash_table.DataTable(id='table2',
+                             data=tableTop.to_dict('records'),
+                             columns=[{"name": i, "id": i} for i in tableTop.columns]
+                             ),
+
+        html.Br(),
+        html.Div([html.H3(children='Gráfico - Retorno % no período')]
+                 , className='banner1'),
+        dcc.RadioItems(
+            ['diário', '30 dias', '180 dias', 'no Ano'],
+            'diário',
+            id='option_graph',
+            inline=True
+        ),
+        html.Button(id='botaoGraph', n_clicks=0, children='Gerar Gráfico'),
+        html.Div(id='figuradiv', children=[]),
+        #html.Div(id='figuradiv', children=[dcc.Graph(figure=fig)]),
+
+        html.Br(),
+        html.Div([html.H3(children='Tweets')]
+                 , className='banner1'),
 
         html.Div([
-            dcc.Location(id='url', refresh=False),
-            html.Div([
-                dcc.Link('Analise|', href='/pages/firstPage'),
-                dcc.Link('Teste|', href='/pages/secondPage'),
-            ], className="banner2"),
-
+            dcc.Dropdown(id='tweetDropdown', options=['Todos','SIMH3','EGIE3','ALUP11','TAEE11','ENBR3','BBSE3','PSSA3','VBBR3','ITSA4','BBAS3','BRSR6','JBSS3','VIVT3','JHSF3'], multi=True)
         ]),
+        html.Button(id='botaoTweets', n_clicks=0, children='Exibir Tweets'),
+        html.Div(id='tweetsdiv', children=[]),
 
-        html.Div(id='page-content', children=[]),
+        html.Div([html.H3(children='Admin')]
+                 , className='banner1'),
+        html.Div(['Senha:',
+                  dcc.Input(id='inputSenha', value='******', type='password'),
+                  html.Button(id='botaoSenha', n_clicks=0, children='Atualizar Dados')
+                  ,
+                  html.Br(),
+                  dcc.RadioItems(
+                      ['1', '2', '3', '4'],
+                      '1',
+                      id='option_tdiv',
+                      inline=True
+                  ),
+                  ])
+        ,
+
+        html.Div(id='hiddendiv', children=[], style={'display': 'none'})
 
     ])
 
 
 app.layout = serve_layout
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/pages/secondPage':
-        return secondPage.layout
-    elif pathname == '/pages/firstPage':
-        return firstPage.layout
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
-
 @app.callback(
-    Output('figuradiv', 'children'),
+    Output('figuradiv','children'),
     Input('botaoGraph', 'n_clicks'),
     State('option_graph', 'value'),
     prevent_initial_call=True
@@ -89,8 +147,9 @@ def update_graph(n_clicks, valor):
 
     return html.Div([dcc.Graph(figure=px.line(figura))])
 
-@callback(
-    Output('hiddendiv', 'children'),
+
+@app.callback(
+    Output('hiddendiv','children'),
     Input('botaoSenha', 'n_clicks'),
     State('inputSenha', 'value'),
     State('option_tdiv', 'value'),
@@ -105,14 +164,15 @@ def update_tdiv_and_graph(n_clicks, value, radiovalue):
         tdiv = tdiv[tdiv['margemGordon'] > -5]
         tdiv = gerarTdiv(int(radiovalue), data_cias)
 
-        # manipulatecsv.testarArquivo(f'{actual_dir}/data/teste.txt')
+        #manipulatecsv.testarArquivo(f'{actual_dir}/data/teste.txt')
 
         return html.Div(['Done!'])
 
     elif len(value) == 0:
         raise exceptions.PreventUpdate
 
-@callback(
+
+@app.callback(
     Output('dataframe_output', 'children'),
     Input('botaoInput', 'n_clicks'),
     State('botaoValor', 'value'),
@@ -138,7 +198,7 @@ def update_table(n_clicks, value):
     elif len(value) == 0:
         raise exceptions.PreventUpdate
 
-@callback(
+@app.callback(
     Output('tweetsdiv', 'children'),
     Input('botaoTweets', 'n_clicks'),
     State('tweetDropdown', 'value'),
@@ -159,12 +219,16 @@ def update_output2(n_clicks, value):
     child = html.Div([
 
         dash_table.DataTable(
-            id='table',
-            data=dataTw.to_dict('records'),
-            columns=[{"name": i, "id": i} for i in dataTw.columns],
-        )
+                id='table',
+                data=dataTw.to_dict('records'),
+                columns=[{"name": i, "id": i} for i in dataTw.columns],
+            )
     ])
     return child
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
+
 '''
 
     actual_dir = pathlib.Path().absolute()
--- test7A.py
+++ test7A.py
@@ -1,14 +1,16 @@
 import analisador
 import datetime
+import dash
 import pandas as pd
 import pathlib
-from dash import Dash, html, dcc, dash_table, State, exceptions
+from dash import Dash, html, dcc, dash_table, State, exceptions, callback
 from dash.dependencies import Input, Output
 import plotly.express as px
 import graphViewer as gv
 import plotly.graph_objs as go
-from pages import secondPage
+from pages import secondPage, firstPage
 
+import manipulatecsv
 
 def gerarTable(tdiv, valor):
     dadosTable = analisador.distribuirAporte(tdiv.copy(), valor)
@@ -33,106 +35,28 @@ def viewTableTop(tdiv):
     #return gerarTable(tdiv.head(int(len(tdiv) // 1.5)), 1000)
 
 
-app = Dash(__name__)
+app = Dash(__name__, suppress_callback_exceptions=True)
 server = app.server #server heroku reconhecer a app
 
 
-actual_dir = pathlib.Path().absolute()
-
-path = f'{actual_dir}/data/statusinvest-busca-avancada.csv'
-data_cias = analisador.lerCsv(path)
-tdiv = pd.DataFrame()
-figura = None
-
 def serve_layout():
-    global tdiv
-    global figura
-    global data_cias
-
-    tdiv = gerarTdiv(1, data_cias)
-#   fig = px.line(figura)
-
-    tableAll = viewTableAll(tdiv)
-    tableTop = viewTableTop(tdiv)
-
 
     return html.Div(children=[
         html.Div([html.H1(children='DATA FINANCE')]
                  , className='banner'
                  )
         ,
+
         html.Div([
             dcc.Location(id='url', refresh=False),
-            html.Div([html.H2(
-                dcc.Link('Análise|', href='/pages/secondPage'))
+            html.Div([
+                dcc.Link('Analise|', href='/pages/firstPage'),
+                dcc.Link('Teste|', href='/pages/secondPage'),
             ], className="banner2"),
+
         ]),
+
         html.Div(id='page-content', children=[]),
-        html.Div([html.H3(children='Top')]
-                 , className='banner1'),
-        html.Div(['valor do Aporte:',
-                  dcc.Input(id='botaoValor', value='1000', type='text'),
-                  html.Button(id='botaoInput', n_clicks=0, children='Atualizar')
-                  ])
-        ,
-        html.Div(id='dataframe_output',
-                 children=html.Div([
-                                    dash_table.DataTable(
-                                        id='table',
-                                        data=tableAll.to_dict('records'),
-                                        columns=[{"name": i, "id": i} for i in tableAll.columns],
-                                    )
-                                    ])
-                 )
-        ,
-        html.Br(),
-        html.Div([html.H3(children='Top +')]
-                 , className='banner1'),
-        dash_table.DataTable(id='table2',
-                             data=tableTop.to_dict('records'),
-                             columns=[{"name": i, "id": i} for i in tableTop.columns]
-                             ),
-
-        html.Br(),
-        html.Div([html.H3(children='Gráfico - Retorno % no período')]
-                 , className='banner1'),
-        dcc.RadioItems(
-            ['diário', '30 dias', '180 dias', 'no Ano'],
-            'diário',
-            id='option_graph',
-            inline=True
-        ),
-        html.Button(id='botaoGraph', n_clicks=0, children='Gerar Gráfico'),
-        html.Div(id='figuradiv', children=[]),
-        #html.Div(id='figuradiv', children=[dcc.Graph(figure=fig)]),
-
-        html.Br(),
-        html.Div([html.H3(children='Tweets')]
-                 , className='banner1'),
-
-        html.Div([
-            dcc.Dropdown(id='tweetDropdown', options=['Todos','SIMH3','EGIE3','ALUP11','TAEE11','ENBR3','BBSE3','PSSA3','VBBR3','ITSA4','BBAS3','BRSR6','JBSS3','VIVT3','JHSF3'], multi=True)
-        ]),
-        html.Button(id='botaoTweets', n_clicks=0, children='Exibir Tweets'),
-        html.Div(id='tweetsdiv', children=[]),
-
-        html.Div([html.H3(children='Admin')]
-                 , className='banner1'),
-        html.Div(['Senha:',
-                  dcc.Input(id='inputSenha', value='******', type='password'),
-                  html.Button(id='botaoSenha', n_clicks=0, children='Atualizar Dados')
-                  ,
-                  html.Br(),
-                  dcc.RadioItems(
-                      ['1', '2', '3', '4'],
-                      '1',
-                      id='option_tdiv',
-                      inline=True
-                  ),
-                  ])
-        ,
-
-        html.Div(id='hiddendiv', children=[], style={'display': 'none'})
 
     ])
 
@@ -144,9 +68,14 @@ app.layout = serve_layout
 def display_page(pathname):
     if pathname == '/pages/secondPage':
         return secondPage.layout
+    elif pathname == '/pages/firstPage':
+        return firstPage.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
 
 @app.callback(
-    Output('figuradiv','children'),
+    Output('figuradiv', 'children'),
     Input('botaoGraph', 'n_clicks'),
     State('option_graph', 'value'),
     prevent_initial_call=True
@@ -160,9 +89,8 @@ def update_graph(n_clicks, valor):
 
     return html.Div([dcc.Graph(figure=px.line(figura))])
 
-
-@app.callback(
-    Output('hiddendiv','children'),
+@callback(
+    Output('hiddendiv', 'children'),
     Input('botaoSenha', 'n_clicks'),
     State('inputSenha', 'value'),
     State('option_tdiv', 'value'),
@@ -177,15 +105,14 @@ def update_tdiv_and_graph(n_clicks, value, radiovalue):
         tdiv = tdiv[tdiv['margemGordon'] > -5]
         tdiv = gerarTdiv(int(radiovalue), data_cias)
 
-        #manipulatecsv.testarArquivo(f'{actual_dir}/data/teste.txt')
+        # manipulatecsv.testarArquivo(f'{actual_dir}/data/teste.txt')
 
         return html.Div(['Done!'])
 
     elif len(value) == 0:
         raise exceptions.PreventUpdate
 
-
-@app.callback(
+@callback(
     Output('dataframe_output', 'children'),
     Input('botaoInput', 'n_clicks'),
     State('botaoValor', 'value'),
@@ -211,7 +138,7 @@ def update_table(n_clicks, value):
     elif len(value) == 0:
         raise exceptions.PreventUpdate
 
-@app.callback(
+@callback(
     Output('tweetsdiv', 'children'),
     Input('botaoTweets', 'n_clicks'),
     State('tweetDropdown', 'value'),
@@ -232,16 +159,12 @@ def update_output2(n_clicks, value):
     child = html.Div([
 
         dash_table.DataTable(
-                id='table',
-                data=dataTw.to_dict('records'),
-                columns=[{"name": i, "id": i} for i in dataTw.columns],
-            )
+            id='table',
+            data=dataTw.to_dict('records'),
+            columns=[{"name": i, "id": i} for i in dataTw.columns],
+        )
     ])
     return child
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
-
 '''
 
     actual_dir = pathlib.Path().absolute()
